81a80f68044e2bc9c7ae246e9bf63aff3e2d7984
david <davidlacustodio@gmail.com>
2016-06-22 10:56:53 +0200

    direct communication with context broker

---
+++ entity-config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by David on 03/06/2016.
+ */
+
+var entity = {
+    "id": "viur-vessel",
+    "type": "Tessel",
+    "temperature": {
+        "value": 23,
+        "type": "Float"
+    },
+    "pressure": {
+        "value": 720,
+        "type": "Integer"
+    },
+    context_broker: 'http://atp.viurdata.com:1026'
+};
+
+module.exports = entity;
--- index.js
+++ index.js
@@ -2,24 +2,122 @@
 var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
 var climatelib = require('climate-si7020');
+//var lwtm2m = require('lwm2m-node-lib');
+var config = require('./config');
+var request = require('request');
+var entity = require('./entity-config');
+
+ 
 
 var ambient = ambientlib.use(tessel.port['A']);
 var climate = climatelib.use(tessel.port['B']);
+
+//lwtm2m.client.init(config);
+
+
+request(entity.context_broker + '/version', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var res = JSON.parse(body);
+        console.log(res) 
+    }
+});
+
+
+
+
+/*
+//Before making any interaction with a Lightweight M2M server, a client must register to it. This registration can be done with the following function:
+lwtm2m.client.register('127.0.0.1', config.server.port,  'testEndpoint',
+    function (error, deviceInfo) {
+      console.log(error.toString() + "--> "+deviceInfo);
+        lwtm2m.client.registry.create("/d/1");
+
+        lwtm2m.client.registry.get('/d/1', function (error, result) {
+        console.log(error);
+        });
+
+    });
+
+*/
+
+
+/*lwtm2m.start({
+    lifetime: '85671',
+    version: '1.0',
+    logLevel: 'DEBUG',
+    observe: {
+        period: 3000
+    },
+    ipProtocol: 'udp4',
+    serverProtocol: 'udp4',
+    formats: [
+        {
+            name: 'lightweightm2m/text',
+            value: 1541
+        }
+    ],
+    writeFormat: 'lightweightm2m/text'
+}, function(error) {
+    console.log('Listening');
+});*/
+
 
 
 ambient.on('ready', function () {
   // Get points of light and sound data.
   setInterval( function () {
     ambient.getLightLevel( function(err, lightdata) {
+        console.log(err);
       if (err) throw err;
       ambient.getSoundLevel( function(err, sounddata) {
-        if (err) throw err;
-       
-        console.log("Light level:", lightdata.toFixed(8), " ", "Sound Level:", sounddata.toFixed(8));
+          console.log(err);
+
+          if (err) throw err;
+       var date = new Date();
+        console.log(date.toString() +"Light level:", lightdata.toFixed(8), " ", "Sound Level:", sounddata.toFixed(8));
         //tessel.led[2].toggle();
+
+
+          request({
+              uri: entity.context_broker +'/v2/entities/viur-vessel/attrs/light/value',
+              json: false,
+              method: 'PUT',
+              headers: {'Content-Type':'text/plain'},
+              body: sounddata.toFixed(8)
+          }, function (error, response, body) {
+              console.log(response.statusCode);
+
+              if (error) {
+                  //var res = JSON.parse(body);
+                  console.log('error '+error)
+              }
+
+              if(response.statusCode == 204) {
+                  console.log('light sent');
+              }
+          });
+
+          request({
+              uri: entity.context_broker +'/v2/entities/viur-vessel/attrs/sound/value',
+              json: false,
+              method: 'PUT',
+              headers: {'Content-Type':'text/plain'},
+              body: lightdata.toFixed(8)
+          }, function (error, response, body) {
+              console.log(response.statusCode);
+
+              if (error) {
+                  //var res = JSON.parse(body);
+                  console.log('error '+error)
+              }
+
+              if(response.statusCode == 204) {
+                  console.log('sound sent');
+              }
+          });
       });
     });
-  }, 2000); // The readings will happen every .5 seconds
+  }, 15000); // The readings will happen every 15 seconds
 });
 
 ambient.on('error', function (err) {
@@ -31,11 +129,48 @@ climate.on('ready', function(){
     climate.readHumidity(function(err, humid){
       climate.readTemperature('c', function(err, temp){
         
-        console.log('Degrees:', temp.toFixed(4) + 'C', 'Humidity:', humid.toFixed(4) + '%RH');
+        console.log('Degrees:', temp.toFixed(4) + 'C', 'Humidity:', humid.toFixed(2) + '%RH');
         //tessel.led[3].toggle();
+
+          request({
+              uri: entity.context_broker +'/v2/entities/viur-vessel/attrs/temperature/value',
+              json: false,
+              method: 'PUT',
+              headers: {'Content-Type':'text/plain'},
+              body: temp.toFixed(2)
+          }, function (error, response, body) {
+              console.log(response.statusCode);
+
+              if (error) {
+                  //var res = JSON.parse(body);
+                  console.log('error '+error)
+              }
+
+              if(response.statusCode == 204) {
+                  console.log('temperature sent');
+              }
+          });
+
+          request({
+              uri: entity.context_broker +'/v2/entities/viur-vessel/attrs/humidity/value',
+              json: false,
+              method: 'PUT',
+              headers: {'Content-Type':'text/plain'},
+              body: humid.toFixed(2)
+          }, function (error, response, body) {
+              console.log(response.statusCode);
+
+              if (error) {
+                  //var res = JSON.parse(body);
+                  console.log('error '+error)
+              }
+              if(response.statusCode == 204) {
+                  console.log('humidity sent');
+              }
+          });
       });
     });
-  }, 2000);
+  }, 15000);
 });
 
 climate.on('error', function(err) {
